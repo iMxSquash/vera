8fa472ac1dad6be0fd4427b7d74cc6ac25e4185e
Elyes <elyes.bk@orange.fr>
2025-11-25 17:56:02 +0100

    changement environnement.developments

--- apps/frontend/src/environments/environment.development.ts
+++ apps/frontend/src/environments/environment.development.ts
@@ -4,10 +4,9 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
-  backendUrl: 'http://localhost:3000',
-  frontendUrl: 'http://localhost:4200',
-  supabaseApiKey: 'ysb_publishable_19GBrMHjDEKSeaEMxP8eRg_pMOSuKrT',
-  supabaseUrl: 'https://xyz.supabase.co',
+  clientUrl: 'http://localhost:4200',
+  serverUrl: 'http://localhost:3000',
+  supabaseApiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxanhwZHFhY3BwaHB2Z2pkZ2V3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM3MDk5NzYsImV4cCI6MjA3OTI4NTk3Nn0.QuYvnCkyOgEv8ikE2AaP2FVGdSf9qqcDXwIPBoTkxos',
+  supabaseUrl: 'https://cqjxpdqacpphpvgjdgew.supabase.co',
   tokenKey: 'vera_admin_token',
 };
