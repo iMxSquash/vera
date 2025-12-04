e07e6de097e6a919e087acdbaa513169941bc72e
Elwen <coussotelwen@gmail.com>
2025-12-04 11:31:15 +0100

    fix(survey): remove unused import from survey.service.ts and update index.ts exports

--- libs/api/features/survey/src/lib/survey.service.ts
+++ libs/api/features/survey/src/lib/survey.service.ts
@@ -3,7 +3,6 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Survey } from './entities/survey.entity';
 import { GoogleSheetsService } from './google-sheets/google-sheets.services';
-import { error } from 'node:console';
 
 @Injectable()
 export class SurveyService {
